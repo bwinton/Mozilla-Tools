550cc801e788a155ecd581c34937901157d6a3d0
Blake Winton <bwinton@latte.ca>
2011-01-30 22:17:21 -0800

    Change to the Mu template engine, and add links.

--- file-clusterer/server.js
+++ file-clusterer/server.js
@@ -1,28 +1,46 @@
 #!/usr/bin/env node
 
 var http = require("http");
+var mu = require("./lib/mu");
 var redis = require("redis-node");
 var sys = require("sys");
 var url = require("url");
 
-var fileDb = redis.createClient();
+const MAX_RESULTS = 10;  // Set to -1 to get everything.
 const FILE_DB = 2;
+var fileDb = redis.createClient();
 fileDb.select(FILE_DB);
 
+var tmpl = "There are {{count}} results.<br>\n" +
+           "{{#results}}<b><a href='/{{id}}'>{{id}}</a></b><br>\n" +
+             "<ul>{{#values}}" +
+               "<li>{{count}}: {{file}}</li>\n" +
+             "{{/values}}</ul><br>\n" +
+           "{{/results}}";
+var compiled = mu.compileText(tmpl, {});
+
 http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, {"Content-Type": "text/html"});
   var path = url.parse(req.url).pathname.substr(1);
   console.log(path);
-  var done = function() { res.end() };
+  var context = {};
+  var done = function() {
+    compiled(context)
+      .addListener("data", function(c) { res.write(c); })
+      .addListener("end", function() {res.end(); });
+  };
   fileDb.keys("*"+path+"*", function(err, arrayOfKeys) {
     count = arrayOfKeys.length;
+    context.count = count;
+    context.results = [];
     arrayOfKeys.forEach(function(key) {
-      fileDb.zrevrange(key, 0, -1, "withscores", function(err, value) {
-        res.write(key + "<br><ul>\n");
-        value.forEach(function(val) {
-          res.write("<li>"+JSON.stringify(val)+"</li>\n");
+      fileDb.zrevrange(key, 0, MAX_RESULTS, "withscores", function(err, values) {
+        subcontext = {id: key, values: []};
+        values.forEach(function(value) {
+          for (var i in value)
+            subcontext.values.push({file: i, count:value[i]});
         });
-        res.write("</ul>\n");
+        context.results.push(subcontext)
         count--;
         if (count == 0)
           done();
@@ -31,4 +49,4 @@ http.createServer(function(req, res) {
   });
 }).listen(8123, "127.0.0.1");
 
-console.log('Server running at http://127.0.0.1:8123/');
+console.log("Server running at http://127.0.0.1:8123/");
